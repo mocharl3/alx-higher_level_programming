a8baad036712ac8ae18a0b990d1d8f0060698ba6
mocharl3 <mosescharles910@gmail.com>
2023-03-09 07:13:29 -0800

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    n = len(sys.argv)
+    args = sys.argv
+    if n == 1:
+        print("{:d} arguments.".format(n-1))
+    elif n == 2:
+        n -= 1
+        str = sys.argv[n]
+        print("{:d} argument:\n{:d}: {:s}".format(n, n, str))
+    elif n > 2:
+        print("{:d} arguments:".format(n-1))
+        for i in range(1, n):
+            str = sys.argv[i]
+            print("{:d}: {:s}".format(i, str))

9525b7d7edf551b2aba1512bfb1923c6ca7ecb39
mocharl3 <mosescharles910@gmail.com>
2023-04-03 04:50:55 -0700

    More classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Rectangle class to represent a square"""
+
+
+class Rectangle:
+    """
+    Defines a Rectangle and its basic properties
+    """
+    pass
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
